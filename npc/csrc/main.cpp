50f525b6550267e4b9c3b525eb2bd69db5f87483
ysyx_22051013 Huang XinYu <2051006442@qq.com>
2023-01-25 22:21:40 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard 68ffd2115a5944b5e616dad34fd1f838fa8e44ed N4: remove unused pins

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
+int main(int argc, char** argv) {
+	VerilatedContext* contextp = new VerilatedContext;
+	contextp->commandArgs(argc,argv);
+	Vtop* top = new Vtop{contextp};
+	Verilated::traceEverOn(true);
+	VerilatedVcdC* tfp = new VerilatedVcdC;
+	top->trace(tfp,0);
+	tfp->open("obj_dir/Vtop.vcd");
+	while(1){
+		int a = rand() & 1;
+		int b = rand() & 1;
+		top->a = a;
+		top->b = b;
+		top->eval();
+		contextp->timeInc(1);
+		tfp->dump(contextp->time());
+		printf("a = %d, b = %d, f = %d\n",a,b,top->f);
+		assert(top->f == (a^b));}
+	tfp->close();
+	delete top;
+	delete contextp;
+
+ // printf("Hello, ysyx!\n");
   return 0;
 }
